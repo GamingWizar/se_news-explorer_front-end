4141cfb1d955efb537e5517dd9c1e9bdf5619331
Carver Hannasch <awizard1011@gmail.com>
2024-11-21 15:09:15 -0500

    add saved cards section, and create template for news card and new cards list

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,11 +1,37 @@
 import "./SavedNews.css";
 
 import SavedNewsHeader from "../SavedNewsHeader/SavedNewsHeader";
+import NewsCardList from "../NewsCardList/NewsCardList";
+import placeHolderImg from "../../assets/card_placeholder.png";
 
 export default function SavedNews(props) {
   return (
-    <>
+    <div className=" page__section saved-news">
       <SavedNewsHeader />
-    </>
+      <NewsCardList
+        isLoggedIn={false}
+        isSaved={false}
+        cards={[
+          {
+            img: placeHolderImg,
+            title: "Carver has the best Birb",
+            date: "November 21, 2024",
+            body: "Carver really has the best Birb named Soren, you need to see how cute he is",
+            author: "WTWR",
+            keyword: "Facts",
+            id: "1789",
+          },
+          {
+            img: placeHolderImg,
+            title: "Carver has the best Girlfriend Easily",
+            date: "November 21, 2024",
+            body: "Carver really has the best girlfriend Rachel, she is so amazing and wonderful and honestly just perfect for him. Carver couldn't possibly image a better girlfriend because one just simply couldn't exist. He loves Rachel more than anything else in the entire world",
+            author: "WTWR",
+            keyword: "The Truth",
+            id: "17898",
+          },
+        ]}
+      />
+    </div>
   );
 }
