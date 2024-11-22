a506e4a68278ccc090c30d53f222e5224648a359
Carver Hannasch <awizard1011@gmail.com>
2024-11-22 16:58:10 -0500

    add searched news section and add logged in and dark states for header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
@@ -10,19 +12,50 @@ import Footer from "../Footer/Footer";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [hasSearched, setHasSearched] = React.useState(false);
+  const [useDarkTheme, setUseDarkTheme] = React.useState(false);
+  const [currentUser, setCurrentUser] = React.useState({
+    name: "Carver",
+    email: "example-email.com",
+    _id: "12345",
+  });
+  const [currentRoute, setCurrentRoute] = React.useState("Home");
+
+  const onSearch = (evt) => {
+    evt.preventDefault();
+    setHasSearched(true);
+    //Search with API
+  };
 
   return (
-    <>
-      <Header isLoggedIn={isLoggedIn} />
+    <CurrentUserContext.Provider value={{ currentUser }}>
+      <Header
+        isLoggedIn={isLoggedIn}
+        useDarkTheme={useDarkTheme}
+        activeTab={currentRoute}
+        setCurrentRoute={setCurrentRoute}
+      />
       <Routes>
-        <Route path="/" element={<Main isLoggedIn={isLoggedIn} />} />
+        <Route
+          path="/"
+          element={
+            <Main
+              isLoggedIn={isLoggedIn}
+              hasSearched={hasSearched}
+              currentRoute={currentRoute}
+              onSearch={onSearch}
+            />
+          }
+        />
         <Route
           path="/saved-news"
-          element={<SavedNews isLoggedIn={isLoggedIn} />}
+          element={
+            <SavedNews isLoggedIn={isLoggedIn} currentRoute={currentRoute} />
+          }
         />
       </Routes>
       <Footer />
-    </>
+    </CurrentUserContext.Provider>
   );
 }
 
