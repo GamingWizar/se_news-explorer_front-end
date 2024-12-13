bfab2900156e07d0e230a4ad974432da247495e0
Carver Hannasch <awizard1011@gmail.com>
2024-12-13 13:51:06 -0500

    fix website layout at different resolutions, changed about text to be multiple paragraphs instead of using line breaks, and added alt text to images that were missing it

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,21 +5,22 @@ export default function About(props) {
   return (
     <section className="page__section about">
       <div className="about__content">
-        <img className="about__image" src={aboutImg} />
+        <img className="about__image" src={aboutImg} alt="Website Author" />
         <div className="about__text-content">
           <h3 className="about__title">About the author</h3>
-          <p className="about__text">
+          <p className="about__text about__text_first">
             Hello, my name is Carver. I'm a new software engineer with knowledge
             in HTML, CSS, JavaScript, and React. I've always loved programming,
             whether it was making mods for video games, or even trying to make
             my own games.
-            <br />
-            <br />I have taken TripleTen's Software Engineer course, which was
-            exactly what I needed to turn my love for programming into a proper
-            skillset and to become a professional software engineer. I now
-            possess the capabilities to turn anyone's ideas or design into a
-            fully functional website, front end and back end, working with them
-            to get down exactly what they want.
+          </p>
+          <p className="about__text">
+            I have taken TripleTen's Software Engineer course, which was exactly
+            what I needed to turn my love for programming into a proper skillset
+            and to become a professional software engineer. I now possess the
+            capabilities to turn anyone's ideas or design into a fully
+            functional website, front end and back end, working with them to get
+            down exactly what they want.
           </p>
         </div>
       </div>
